72aee3e7eb91bc6b9bf4471c779a3fc495342b52
Alexander <aleyva@recurly.com>
2022-12-04 01:58:11 -0300

    update day 03

--- day03/script.rb
+++ day03/script.rb
@@ -5,10 +5,7 @@ PRIORITY = ('a'..'z').to_a + ('A'..'Z').to_a
 parse = -> (x) { x.split('')  }
 items = -> (x) { x.each_slice(x.length / 2).to_a }
 shared = -> (xs) { xs.reduce(&:&) }
-first = -> (x) { x.first }
 priority = -> (x) { PRIORITY.index(x) + 1 }
 
-map = -> (x, *f) { f.reduce(x) { |x, fi| x.map(&fi) } }
-
-puts map.(INPUT, parse, items, shared, first, priority).sum
-puts map.(map.(INPUT, parse).each_slice(3), shared, first, priority).sum
+puts INPUT.map(&(parse >> items >> shared >> :first.to_proc >> priority)).sum
+puts INPUT.map(&parse).each_slice(3).map(&(shared >> :first.to_proc >> priority)).sum
